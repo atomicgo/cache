52bdb487b160565146264176522c1e5462221fb5
Marvin Wendt <git@marvinjwendt.com>
2022-11-25 18:50:17 +0100

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,9 +1,4 @@
 /*
-Package template is used to generate new AtomicGo repositories.
-
-Write the description of the module here. You can use **markdown**!
-This description should clearly explain what the package does.
-
-Example description: https://golang.org/src/encoding/gob/doc.go
+Package cache is a generic, fast and thread-safe cache implementation to improve performance of your Go applications.
 */
 package cache
